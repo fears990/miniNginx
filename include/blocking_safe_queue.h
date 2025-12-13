73d0b2126d8c9803fb2a9858d11573887482398e
Alex <96337677+fears990@users.noreply.github.com>
2025-12-13 19:43:07 +0300

    move template metods

--- include/blocking_safe_queue.h
+++ include/blocking_safe_queue.h
@@ -17,16 +17,65 @@ public:
     blocking_thread_safe_queue(blocking_thread_safe_queue&&) noexcept            = default;
     blocking_thread_safe_queue& operator=(blocking_thread_safe_queue&&) noexcept = default;
 
-    void push(T value);
+    void push(T value)
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_data.push(std::move(value));
+        m_cv.notify_one();
+    }
 
     template<typename... Args>
-    void emplace(Args&&... args);
+    void emplace(Args&&... args)
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_data.emplace(std::forward<Args>(args)...);
+        m_cv.notify_one();
+    }
 
-    std::optional<T> wait_and_pop();
-    bool             try_pop(T& value);
-    size_t           get_size() const;
-    bool             empty() const;
-    void             shutdown();
+    std::optional<T> wait_and_pop()
+    {
+        std::unique_lock u_lock(m_mutex);
+        m_cv.wait(u_lock, [this]() { return !m_data.empty() || m_shutdown; });
+        if (m_data.empty() && m_shutdown)
+        {
+            return std::nullopt;
+        }
+
+        T tmp = std::move(m_data.front());
+        m_data.pop();
+        return tmp;
+    }
+
+    bool try_pop(T& value)
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        if (m_data.empty())
+        {
+            return false;
+        }
+
+        value = std::move(m_data.front());
+        m_data.pop();
+        return true;
+    }
+
+    size_t get_size() const
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        return m_data.size();
+    };
+    bool empty() const
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+
+        return m_data.empty();
+    };
+    void shutdown()
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_shutdown = true;
+        m_cv.notify_all();
+    };
 
 private:
     std::queue<T>           m_data;
@@ -34,3 +83,4 @@ private:
     mutable std::mutex      m_mutex;
     bool                    m_shutdown = false;
 };
+
